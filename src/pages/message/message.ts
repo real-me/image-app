2b3a611c7c883d5dfb0d22fb0a5b9521ae5dc5eb
real-me <1960869869@qq.com>
2017-08-18 15:23:21 +0800

    1.个人中心 2.我的消息 3.消息详情

---
+++ src/pages/message/message.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {UtilProvider} from "../../providers/util/util";
+
+@IonicPage()
+@Component({
+  selector: 'page-message',
+  templateUrl: 'message.html',
+})
+export class MessagePage {
+
+  refresh: () => void;
+  data:any=null;
+  id: number = 0;
+  hasInit:boolean=false;//是否已经初始化完毕
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private util: UtilProvider) {
+    this.initData();
+    this.refresh = () => {
+      this.clear();//清除数据
+      this.util.checkLogin().then(isLogin => {
+        isLogin && this.init();
+      });
+    };
+  }
+
+  //-----------------需要登录的页面必须有的方法(START)
+  ionViewDidLoad() {
+    this.util.setPage(this);
+    this.util.reInitIcon();
+    this.initListener();//初始化事件侦听
+    this.refresh();
+  }
+
+  //页面移除时删除事件侦听
+  viewWillUnload() {
+    this.removeListener();
+  }
+
+  //初始化
+  init() {
+    this.util.loading();
+    Promise.all([this.loadData()]).then(() => this.util.hideLoading()).catch(() => this.util.hideLoading());
+  }
+
+  //清除数据(用于刷新)
+  clear() {
+    this.data = null;
+    this.hasInit = false;
+  }
+
+  //初始化事件侦听
+  initListener() {
+
+  }
+
+  //删除事件侦听
+  removeListener() {
+
+  }
+
+  doRefresh(refresher) {
+    refresher.complete();
+    setTimeout(this.refresh, this.util.refreshDuration);
+  }
+
+  //-----------------需要登录的页面必须有的方法(END)
+
+  //获取页面传递过来的参数
+  initData() {
+    this.id = this.navParams.get('id');
+
+  }
+
+  //加载数据
+  loadData(){
+    this.hasInit = true;
+    return Promise.resolve(true);
+  }
+
+}
